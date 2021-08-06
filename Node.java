0ef0f72fa6a97f287f14fe39c4bdb4d89bbad0cc
Zentai Marton <marton.zentai@minero.hu>
2021-08-06 21:13:45 +0200

    Startad Node class, barely any code written

---
+++ Node.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    List<Node> adj = new ArrayList<>();
+    int id = -1;
+    Node parent = null;
+    int dist = 0;
+
+    public Node() {}
+
+
+}
